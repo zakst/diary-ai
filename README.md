c898b12969a6dbcca72ae052a3499bee3243be7c
zakibadawi <zaba@ganintegrity.com>
2025-05-23 20:42:56 +0200

    feat: added qdrant repository

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ Create a `.env` file
 QDRANT_URL=qdrant_endpoint # you can find it in the cluster
 QDRANT_API_KEY=api_key
 QDRANT_COLLECTION=collection_name # defaults to diaries
+OPEN_API_KEY=api_key
 ```
---
+++ types/diary_types.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from typing import TypedDict, List
+
+class EntryType(str, Enum):
+    TEXT = "text"
+
+class EntrySource(str, Enum):
+    LOCAL_IMPORT = "local_import"
+
+class DiaryEntry(TypedDict):
+    user_uuid: str
+    type: EntryType
+    source: EntrySource
+    content: str
+    mime_type: str
+    tags: List[str]
+    created_at: int
+    updated_at: int
